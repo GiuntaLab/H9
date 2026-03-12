66eecf9b206655f1941e3aa4076442ae58879ae4
Matteo <98895614+Overcraft90@users.noreply.github.com>
2026-03-12 20:40:59 +0100

    merqury script

--- Genome quality/01_merqury.sh
+++ Genome quality/01_merqury.sh
@@ -3,7 +3,7 @@
 ###This script was run as an array job, it also### 
 ###assumes a DB .meryl has been already created###
 
-HAP= "$(echo ${d} | cut -f 7 -d '/' | sed 's/[a-z,A-Z,0-9]\+_//' | sed 's/[A-Z,0-9]\+_//')" #this based on folder structure and naming will produce either "ref" or "hap"
+HAP="$(echo ${d} | cut -f 7 -d '/' | sed 's/[a-z,A-Z,0-9]\+_//' | sed 's/[A-Z,0-9]\+_//')" #this based on folder structure and naming will produce either "ref" or "hap"
 
 source /miniconda3/24.4.0/bin/activate merqury
 if [ "$HAP" == "ref" ]; then

10a1518f9688e8e51dc7aa2d2f81961fd7ba6a04
Matteo <98895614+Overcraft90@users.noreply.github.com>
2026-03-12 19:51:46 +0100

    Update 01_merqury.sh

--- Genome quality/01_merqury.sh
+++ Genome quality/01_merqury.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+###This script was run as an array job, it also### 
+###assumes a DB .meryl has been already created###
+
+HAP="$(echo ${d} | cut -f 7 -d '/' | sed 's/[a-z,A-Z,0-9]\+_//' | sed 's/[A-Z,0-9]\+_//')" #this based on folder structure and naming will produce either "ref" or "hap"
+
+source /miniconda3/24.4.0/bin/activate merqury
+if [ "$HAP" == "ref" ]; then
+	cd $d/merqury; merqury.sh ${ID}.meryl ../*.fna.gz merqury_${ID}
+else
+	cd $d/merqury; merqury.sh ${ID}.meryl ../hap1/*.fasta.gz ../hap2/*.fasta.gz merqury_${ID}
+fi
+conda deactivate

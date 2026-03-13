5a114fc62c710965904b0d58ed912cb8689802a8
Matteo <98895614+Overcraft90@users.noreply.github.com>
2026-03-13 13:29:28 +0100

    pangenome by chromosome script

---
+++ Pangenome PCA and ancestry analyses/01_pan_pca-build_pangenomes_by_chromosome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#run PGGB pangenome
+source ~/miniconda3/bin/activate pggb
+	pggb -i $d/pangenome/samples_all.fna.gz -o $d/pangenome -t 256 -p 50 -s 1000 -n 334 -k 311 -O 0.03 -T 128 -G 13117,13219 -S -m -v -V 'CHM13:#'
+conda deactivate
